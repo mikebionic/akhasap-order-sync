b87110d59b3163129014c04230b9fcd902668d0b
mike-bionic <muhammedjepbarov@gmail.com>
2021-06-19 21:19:00 +0500

    updated api to use updated guids

--- main/models/Warehouse.py
+++ main/models/Warehouse.py
@@ -5,7 +5,7 @@ from main import db
 class Warehouse(db.Model):
 	__tablename__ = "tbl_mg_whouse"
 	WhId = db.Column("wh_id",db.Integer,nullable=False,primary_key=True)
-	WhGuid = db.Column("wh_id_guid",UUID(as_uuid=True),unique=True)
+	WhGuid = db.Column("WhGuid",UUID(as_uuid=True),unique=True)
 	WhName = db.Column("wh_name",db.String)
 
 	def to_json(self):
